5b481cc21a9bb5817ab0c6ff3b7ab31c899751e6
RealSpiderParker <realspiderparker18627@gmail.com>
2024-12-10 16:54:16 -0600

    pid...

--- road-runner-quickstart-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/PID2ndTest.java
+++ road-runner-quickstart-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/PID2ndTest.java
@@ -3,31 +3,46 @@ package org.firstinspires.ftc.teamcode.test;
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.arcrobotics.ftclib.controller.PIDController;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 
 
 @Config
 @TeleOp
-public class PID2ndTest extends OpMode {
+public class PID2ndTest extends LinearOpMode {
+    //all the "private" stuff
+    // double multiplier;
+    //    YawPitchRollAngles_7ByawPitchRollAnglesVariable_7D;
+    //    double rotX;
+    //    float x;
+    //    double rotY;
+    //    float y;
+    //private DcMotor FrontL,BackL, FrontR,BackR,Actuator,ExtendMoto;
     private PIDController controller;
+private IMU imu_IMU;
+    private DcMotorEx motor1,motor2;
 
-    public static double p = 0, i = 0, d = 0;
-    public static double f = 0;
-
+    public static double p = .009, i = 0, d = .0001;
+    public static double f = .1;
     public static int target = 0;
     private final double ticks_in_degrees = 780 / 180.0;
-     private DcMotorEx motor1,motor2;
+
 
     @Override
     public void init() {
 controller = new PIDController(p, i, d);
 telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
-
 motor1 = hardwareMap.get(DcMotorEx.class, "LeftM");
 motor2 = hardwareMap.get(DcMotorEx.class, "RightM");
+motor2.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
     @Override
