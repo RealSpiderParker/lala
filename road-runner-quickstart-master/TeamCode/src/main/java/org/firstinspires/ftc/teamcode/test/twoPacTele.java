08fab9a2ccba5f35ce742ed6d318ca683016407d
RealSpiderParker <realspiderparker18627@gmail.com>
2025-01-27 22:21:30 -0600

    pid...

--- road-runner-quickstart-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/twoPacTele.java
+++ road-runner-quickstart-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/twoPacTele.java
@@ -1,29 +1,21 @@
 package org.firstinspires.ftc.teamcode.test;
 
 import com.acmerobotics.dashboard.FtcDashboard;
-import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.arcrobotics.ftclib.controller.PIDController;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.ServoImplEx;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 
-@Config
-@TeleOp(name = "PacTele")
-public class PacTele extends LinearOpMode {
-
+public class twoPacTele extends LinearOpMode {
     private IMU imu;
-    private DcMotor bLeft,bRight,fLeft,fRight,armL,armR,;
+    private DcMotor bLeft,bRight,fLeft,fRight,armL,armR,EM1;
     private Servo rCl,lCl,intakeCl,wrist,rPiv,lPiv,scoop;
 
     double multiplier;
@@ -31,21 +23,16 @@ public class PacTele extends LinearOpMode {
     double rotX;
     float x;
     float rx;
-    ///////////////////////////////////////////////////
     double rotY;
     float y;
 
-    //pid stuff..
+    //PID stuff...
     private PIDController controller;
     public static double p = 0.009, i = 0, d = 0.0001;
     public static double f = 0.1;
-    public static int target = 975;
+    public static int target = 0;
     private final double ticks_in_degree = 700 / 180.0;
-///////////////////////////////////////////////////////////////
-    }
-    /**
-     * This function is executed when this Op Mode is selected from the Driver Station.
-     */
+
     @Override
     public void runOpMode() {
         imu = hardwareMap.get(IMU.class, "imu");
@@ -66,20 +53,8 @@ public class PacTele extends LinearOpMode {
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
         controller = new PIDController(p,i,d);
 
-        // Initializes the IMU with non-default settings. To use this block,
-        // plug one of the "new IMU.Parameters" blocks into the parameters socket.
-        // Creates a Parameters object for use with an IMU in a REV Robotics Control Hub or
-        // Expansion Hub, specifying the hub's orientation on the robot via the direction that
-        // the REV Robotics logo is facing and the direction that the USB ports are facing.
         imu.initialize(new IMU.Parameters(new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.RIGHT, RevHubOrientationOnRobot.UsbFacingDirection.UP)));
-        // Reverse one of the drive motors.
-        // You will have to determine which motor to reverse for your robot.
-        // In this example, the right motor was reversed so that positive
-        // applied power makes it move the robot in the forward direction.
         fLeft.setDirection(DcMotor.Direction.REVERSE);
-        // You will have to determine which motor to reverse for your robot.
-        // In this example, the right motor was reversed so that positive
-        // applied power makes it move the robot in the forward direction.
         bLeft.setDirection(DcMotor.Direction.REVERSE);
         EM1.setDirection(DcMotorSimple.Direction.REVERSE);
         armL.setDirection(DcMotor.Direction.REVERSE);
@@ -87,6 +62,7 @@ public class PacTele extends LinearOpMode {
         bRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         fLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         bLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
         waitForStart();
         if (opModeIsActive()) {
 
@@ -142,7 +118,6 @@ public class PacTele extends LinearOpMode {
                 } else {
                     scoop.setPosition(1);
                 }
-
                 gamepad();
                 setPower();
                 // Pressing Right Bumper will Reset the IMU for Field Centric
@@ -156,10 +131,6 @@ public class PacTele extends LinearOpMode {
             }
         }
     }
-
-    /**
-     * Describe this function...
-     */
     private void gamepad() {
         double theta;
 
@@ -174,10 +145,6 @@ public class PacTele extends LinearOpMode {
         rotX = x * Math.cos(theta / 180 * Math.PI) - y * Math.sin(theta / 180 * Math.PI);
         rotY = x * Math.sin(theta / 180 * Math.PI) + y * Math.cos(theta / 180 * Math.PI);
     }
-
-    /**
-     * Describe this function...
-     */
     private void setPower() {
         // The Y axis of a joystick ranges from -1 in its topmost position to +1 in its bottommost position.
         // We negate this value so that the topmost position corresponds to maximum forward power.
@@ -188,10 +155,6 @@ public class PacTele extends LinearOpMode {
         bLeft.setPower(((rotY - rotX) + rx) * multiplier);
         bRight.setPower(((rotY + rotX) - rx) * multiplier);
     }
-
-    /**
-     * Describe this function...
-     */
     private void data() {
         telemetry.addData("IMU Yaw:", _7ByawPitchRollAnglesVariable_7D.getYaw(AngleUnit.DEGREES));
         telemetry.addData("rotX", rotX);
@@ -206,3 +169,4 @@ public class PacTele extends LinearOpMode {
         telemetry.update();
     }
 }
+////this is a new code
